d4dda38f48dc9e70fd4c7d0dc4a55027ff8718d8
StevenG1997 <stivengarcia1855@gmail.com>
2023-03-25 22:39:02 -0500

    change completed change 50

--- src/app/pages/main/contactanos/contactanos.component.ts
+++ src/app/pages/main/contactanos/contactanos.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-contactanos',
   templateUrl: './contactanos.component.html',
   styleUrls: ['./contactanos.component.css']
 })
-export class ContactanosComponent {
+export class ContactanosComponent implements OnInit {
 
+  selectNumber: any = 1;
+
+  number12: number = 12000;
+
+  respuesta: number;
+  texto: any;
+
+  objeto: any;
+  constructor(
+  ) { }
+
+  ngOnInit(): void {
+    this.respuesta = this.number12 * this.selectNumber;
+  }
+
+
+  cambio(e) {
+    this.selectNumber = e.target.value;
+
+    // this.texto = document.getElementById("number4").innerText;
+
+    // console.log(this.texto)
+    console.log("number number" + this.selectNumber)
+
+
+
+    this.respuesta = this.number12 * this.selectNumber;
+
+    console.log("respuestaa", this.respuesta)
+  }
 }
--- src/app/pages/main/gallery/gallery.component.ts
+++ src/app/pages/main/gallery/gallery.component.ts
@@ -1,24 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Galeria } from 'src/app/core/models/galeria';
-
+import Swal from 'sweetalert2';
 // Service
 import { GaleriaService } from 'src/app/core/services/galeria.service';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.css']
+  styleUrls: ['./gallery.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class GalleryComponent implements OnInit {
 
   galeria: any[] = [];
   public page!: number;
+  public pageModal!: number;
 
   galerias: Galeria[];
   selectGroup: any;
+  obtenerInfoAll: any;
+
+  informacion: any;
+  nombre: any;
+  precio: any;
+  id: any;
 
   constructor(
     public router: Router,
@@ -29,6 +37,7 @@ export class GalleryComponent implements OnInit {
 
   ngOnInit(): void {
     this.getGallery();
+    this.obtenerInfo();
   }
 
   getGallery() {
@@ -43,14 +52,64 @@ export class GalleryComponent implements OnInit {
     this.serviceGallery.getBySelectGroup(this.selectGroup).then((datoGroup) => {
       this.galerias = datoGroup;
     });
-    
+
     this.modalService.open(targetModal, {
       centered: true,
       backdrop: 'static',
     });
   }
 
-  compra(e){
-    console.log(e);
+  obtenerInfo() {
+    this.informacion = JSON.parse(localStorage.getItem("informaciondetodo"))
+    // let informacion = JSON.parse(localStorage.getItem("uno"))
+    console.log(this.informacion)
+  }
+
+  guardarInfo(e) {
+    Swal.fire({
+      // title: 'Are you sure?',
+      text: "¿Deseas comprarlo?",
+      icon: 'warning',
+      showCancelButton: true,
+
+      cancelButtonText: 'No',
+      confirmButtonText: 'Si',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.obtenerInfoAll = e.nombre;
+        if (this.obtenerInfoAll != this.informacion) {
+          localStorage.setItem("informaciondetodo", JSON.stringify(this.obtenerInfoAll))
+          // localStorage.setItem("uno", JSON.stringify(all))
+        }
+        // swalWithBootstrapButtons.fire(
+        //   'Deleted!',
+        //   'Your file has been deleted.',
+        //   'success'
+        // )
+      } else if (
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        Swal.fire(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        )
+      }
+    })
+    // let all = {
+    //   nombre: e.nombre,
+    //   precio: e.precio,
+    //   id: e.id
+    // }
+
+
+    // this.nombre = e.nombre
+    // this.precio = e.precio
+    // this.id = e.id
+
+    // localStorage.setItem("nombre", this.nombre);
+    // localStorage.setItem("precio", this.precio);
+    // localStorage.setItem("id", this.id);
   }
 }
