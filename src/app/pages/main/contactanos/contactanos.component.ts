8495583556407c35f84c114186d1cd2ce0e65446
StevenG1997 <stivengarcia1855@gmail.com>
2023-04-08 21:37:50 -0500

    change table Buy 90

--- src/app/pages/main/contactanos/contactanos.component.ts
+++ src/app/pages/main/contactanos/contactanos.component.ts
@@ -22,6 +22,11 @@ export class ContactanosComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  whatssapClick() {
+    const URL2 = 'https://api.whatsapp.com/send/?phone=%2B573164814881&text&type=phone_number&app_absent=0'
+    window.open(URL2, "_blank");
+  }
+
   enviar() {
     const URL = `https://api.whatsapp.com/send?phone=+573043277453&text=Hola.%0A%0AMi nombre es: %20${this.formTemplate.value.name},%20 este es mi numero celular:%20${this.formTemplate.value.phone}%20 y mi correo electronico: %20${this.formTemplate.value.email}&source=&data=`;
     window.open(URL, "_blank");
